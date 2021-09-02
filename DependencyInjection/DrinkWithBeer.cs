8e3ccdd63c6168940e299362d413d019844e76b1
Maxijcari <maxijcari.estudio@gmail.com>
2021-09-02 15:43:51 -0300

    Agregar archivos de proyecto.

---
+++ DependencyInjection/DrinkWithBeer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.DependencyInjection
+{
+    public class DrinkWithBeer
+    {
+        //Este parametro esta mal y es lo que la Inyeccion de dependencia resuelve.
+        private Beer _beer = new Beer("Pikantus","Erdinger");
+        
+        
+        private decimal _water;
+        private decimal _sugar;
+
+        public DrinkWithBeer(decimal water,decimal sugar,Beer beer) 
+        {
+            _water = water;
+            _sugar = sugar;
+            _beer = beer;
+        }
+
+        public void Build() 
+        {
+            Console.WriteLine($"Preparamos bebida que tiene agua {_water} "+$" azúcar {_sugar} y cerveza {_beer.Name}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using DesignPattern.DependencyInjection;
+using DesignPattern.FactoryPattern;
+using System;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var log = Singleton.Log.Instance;
+
+            //log.Save(DateTime.Now.ToString());
+
+            SaleFactory storeSaleFactory = new StoreSaleFactory(10);
+            SaleFactory internetSaleFactory = new InternetSaleFactory(10);
+
+            ISale sale1 = storeSaleFactory.GetSale();
+            sale1.Sell(15);
+
+            ISale sale2 = internetSaleFactory.GetSale();
+            sale2.Sell(15);
+
+            var beer = new Beer("Pikantus","Erdinger");
+            var drinkWithBeer = new DrinkWithBeer(10,1,beer);
+
+            drinkWithBeer.Build();
+
+        }
+    }
+}
